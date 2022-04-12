1c610e0aad3951c44da945998f1b60c49e179a55
ekfkawl <oscgame@naver.com>
2022-04-12 21:53:20 +0900

    bridge enum 추가

--- src/main/java/kr/starbridge/web/domain/bridge/enums/FunctionURIEnum.java
+++ src/main/java/kr/starbridge/web/domain/bridge/enums/FunctionURIEnum.java
@@ -5,8 +5,14 @@ import lombok.Getter;
 @Getter
 public enum FunctionURIEnum {
     URI_BLACKLIST_TAG("blacklist-tag"),
+    URI_BLACKLIST_TAG_IMPORT("blacklist-tag-import"),
+    URI_BLACKLIST_TAG_EXPORT("blacklist-tag-export"),
     URI_BLACKLIST_IP("blacklist-ip"),
+    URI_BLACKLIST_IP_IMPORT("blacklist-ip-import"),
+    URI_BLACKLIST_IP_EXPORT("blacklist-ip-export"),
     URI_ROOM_FILTER("room-filter"),
+    URI_ROOM_FILTER_IMPORT("room-filter-import"),
+    URI_ROOM_FILTER_EXPORT("room-filter-export"),
     URI_ROOM_ROLE("room-role"),
     URI_PLAYER("player");
 
