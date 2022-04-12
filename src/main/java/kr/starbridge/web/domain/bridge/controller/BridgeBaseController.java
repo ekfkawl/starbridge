b5c12c90744b1ff9ad198a92670a8d60ec6b9859
ekfkawl <oscgame@naver.com>
2022-04-12 20:44:14 +0900

    bridge api prefix

---
+++ src/main/java/kr/starbridge/web/domain/bridge/controller/BridgeBaseController.java
@@ -0,0 +1,7 @@
+package kr.starbridge.web.domain.bridge.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/bridge/*")
+public class BridgeBaseController {
+}
