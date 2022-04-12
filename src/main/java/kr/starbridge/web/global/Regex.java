05053c1b3b9ba46ee7534e504b9a5c1cd55b8db3
ekfkawl <oscgame@naver.com>
2022-04-12 20:42:03 +0900

    배틀태그 유효성 정규식 추가

--- src/main/java/kr/starbridge/web/global/Regex.java
+++ src/main/java/kr/starbridge/web/global/Regex.java
@@ -3,4 +3,6 @@ package kr.starbridge.web.global;
 public class Regex {
     /** 공백 제거 */
     public final static String VACUUM = "\\p{Z}";
+    /** 배틀태그 양식 */
+    public final static String BATTLE_TAG = "[0-9ㄱ-ㅎ가-힣A-Za-z]{1,32}\\#[0-9]{4,6}";
 }
