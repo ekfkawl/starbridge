2d6d048287dc8dc1f75996986d44dcd85c8d0f4e
ekfkawl <oscgame@naver.com>
2022-04-12 23:39:44 +0900

    prevtag 수정

--- src/main/resources/static/js/bridge/blacklist-tag.js
+++ src/main/resources/static/js/bridge/blacklist-tag.js
@@ -1,10 +1,9 @@
 var curOption = 0;
-var prevtag = null;
+var prevTag = null;
 
 $(document).on("click", ".edit-button", function() {
     var b = $(this).parent().parent();
-    prevtag = b.children().eq(0).text()
-    console.log(prevtag);
+    prevTag = b.children().eq(0).text()
 });
 
 $(document).on("click", ".save-button", function() {
@@ -14,7 +13,7 @@ $(document).on("click", ".save-button", function() {
     var e = f.eq(1).val();
 
     _post("../bridge/api/battle-tag", {
-        prevTag: prevtag,
+        prev_tag: prevTag,
         id: {
             tag: g
         },
@@ -23,7 +22,7 @@ $(document).on("click", ".save-button", function() {
 });
 
 $(document).on("click", ".discard-button", function() {
-    prevtag = null
+    prevTag = null
 });
 
 $(document).on("click", ".delete-button", function() {
@@ -44,3 +43,20 @@ function getTagsByHash() {
         location.href=[["@{/bridge/import-blacklist-tag}"]] + '?ref=' + input;
     }
 }
+
+$(document).on("click", "#localhash", function() {
+    copyToClipboard($(this).text());
+});
+
+$(document).on("change", ".form-check-input", function() {
+    var c = $(this).parent().parent();
+    var d = c.children().eq(1).text();
+    var e = c.children().eq(0).children().is(":checked") ? true : false;
+
+    _put("../bridge/api/battle-tag-export", {
+        id: {
+            tag: d
+        },
+        is_export: e
+    })
+});
