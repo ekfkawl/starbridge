8711bea56582ae861c31e1389ba0d6928505943c
ekfkawl <oscgame@naver.com>
2022-04-18 16:09:44 +0900

    member dto/mapper 수정

--- src/main/java/kr/starbridge/web/domain/member/dto/MemberDTO.java
+++ src/main/java/kr/starbridge/web/domain/member/dto/MemberDTO.java
@@ -1,21 +1,25 @@
 package kr.starbridge.web.domain.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import kr.starbridge.web.domain.member.enums.RoleEnum;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberDTO {
     /**
      * 아이디
      */
+    @JsonIgnore
     private String id;
     /**
      * 비밀번호
      */
+    @JsonIgnore
     private String pw;
     /**
      * 이름
--- src/main/java/kr/starbridge/web/domain/member/mapper/MemberMapper.java
+++ src/main/java/kr/starbridge/web/domain/member/mapper/MemberMapper.java
@@ -1,5 +1,6 @@
 package kr.starbridge.web.domain.member.mapper;
 
+import kr.starbridge.web.domain.member.dto.MemberDTO;
 import kr.starbridge.web.domain.member.dto.MemberModifyDTO;
 import kr.starbridge.web.domain.member.dto.MemberRegisterDTO;
 import kr.starbridge.web.domain.member.entity.MemberEntity;
@@ -30,6 +31,36 @@ public class MemberMapper {
                 .pw(modifyDTO.getMd5pw())
                 .name(modifyDTO.getName())
                 .auth(modifyDTO.getAuth())
+                .build();
+    }
+
+    /**
+     * MemberDTO -> Entity
+     * @param memberDTO
+     * @return
+     */
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        return MemberEntity.builder()
+                .id(memberDTO.getId())
+                .pw(memberDTO.getPw())
+                .name(memberDTO.getName())
+                .img(memberDTO.getImg())
+                .auth(memberDTO.getAuth())
+                .build();
+    }
+
+    /**
+     * Entity -> MemberDTO
+     * @param memberEntity
+     * @return
+     */
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
+        return MemberDTO.builder()
+                .id(memberEntity.getId())
+                .pw(memberEntity.getPw())
+                .name(memberEntity.getName())
+                .img(memberEntity.getImg())
+                .auth(memberEntity.getAuth())
                 .build();
     }
 }
