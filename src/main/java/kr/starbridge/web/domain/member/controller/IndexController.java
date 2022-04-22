91765432293f76e52320826351c71d48066fbd4a
ekfkawl <oscgame@naver.com>
2022-04-22 21:51:44 +0900

    인덱스 페이지 리뉴얼

--- src/main/java/kr/starbridge/web/domain/member/controller/IndexController.java
+++ src/main/java/kr/starbridge/web/domain/member/controller/IndexController.java
@@ -1,10 +1,13 @@
 package kr.starbridge.web.domain.member.controller;
 
+import kr.starbridge.web.domain.forum.service.ForumContentService;
 import kr.starbridge.web.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.text.NumberFormat;
 
 /**
  * 인덱스 페이지 컨트롤러
@@ -14,13 +17,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 
     private final MemberService memberService;
+    private final ForumContentService forumContentService;
 
     @GetMapping("/")
     public ModelAndView index() {
 
         ModelAndView mv = new ModelAndView("index");
         /** 회원 수 */
-        mv.addObject("memberCount", memberService.getMemberCount());
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        mv.addObject("memberCount", numberFormat.format(memberService.getMemberCount()));
+        mv.addObject("forumContentCount", numberFormat.format(forumContentService.getForumContentCount()));
 
         return mv;
     }
