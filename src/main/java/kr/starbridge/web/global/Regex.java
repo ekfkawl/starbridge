b22257ce85d9717a6bb15a5f2c769ba043b542ba
ekfkawl <oscgame@naver.com>
2022-05-03 16:38:16 +0900

    배틀태그 정규식 수정

--- src/main/java/kr/starbridge/web/global/Regex.java
+++ src/main/java/kr/starbridge/web/global/Regex.java
@@ -4,7 +4,7 @@ public class Regex {
     /** 공백 제거 */
     public final static String VACUUM = "\\p{Z}";
     /** 배틀태그 양식 */
-    public final static String BATTLE_TAG = "[0-9ㄱ-ㅎ가-힣A-Za-z]{1,32}\\#[0-9]{3,8}";
+    public final static String BATTLE_TAG = "[0-9ㄱ-ㅎ가-힣A-Za-z]{0,32}\\#[0-9]{3,8}";
     /** MD5 해시 양식 */
     public final static String MD5_HASH = "^[a-zA-Z0-9]{32,32}$";
     /** 앞 뒤 공백 제거 */
