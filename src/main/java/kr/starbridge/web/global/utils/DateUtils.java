c27e4acf230c3ae2ff30f1055f2aef7b5105e087
ekfkawl <oscgame@naver.com>
2022-05-05 18:35:55 +0900

    날짜 치환 수정

--- src/main/java/kr/starbridge/web/global/utils/DateUtils.java
+++ src/main/java/kr/starbridge/web/global/utils/DateUtils.java
@@ -8,6 +8,8 @@ public class DateUtils {
         long m = ChronoUnit.MINUTES.between(dt, LocalDateTime.now());
         long h = ChronoUnit.HOURS.between(dt, LocalDateTime.now());
         long d = ChronoUnit.DAYS.between(dt, LocalDateTime.now());
+        long mth = ChronoUnit.MONTHS.between(dt, LocalDateTime.now());
+        long y = ChronoUnit.YEARS.between(dt, LocalDateTime.now());
 
         if (m == 0) {
             return "방금";
@@ -15,9 +17,16 @@ public class DateUtils {
         if (m < 60) {
             return String.format("%d분 전", m);
         }
-        if (m < 1440) {
+        if (h > 0 && h < 24) {
             return String.format("%d시간 전", h);
         }
-        return String.format("%d일 전", d);
+        if (d > 0 && mth == 0) {
+            return String.format("%d일 전", d);
+        }
+        if (mth > 0 && y == 0) {
+            return String.format("%d달 전", mth);
+        }
+
+        return String.format("%d년 전", y);
     }
 }
